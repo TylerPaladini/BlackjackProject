93825a344942ce370ae0d44ad3e8c5388c1970d7
Tyler Paladini <TylerPaladini@yahoo.com>
2018-09-15 22:13:15 -0600

    first dealing of both hands

--- src/com/skilldistillery/cards/blackjack/BlackJackApp.java
+++ src/com/skilldistillery/cards/blackjack/BlackJackApp.java
@@ -1,28 +1,52 @@
 package com.skilldistillery.cards.blackjack;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
 import com.skilldistillery.cards.common.Card;
 import com.skilldistillery.cards.common.Deck;
+import com.skilldistillery.cards.common.Rank;
 
 public class BlackJackApp {
-	Dealer deal = new Dealer();
-	static Deck deck = new Deck();
-	
-	
+//	Dealer deal = new Dealer();
+
+
 	public static void main(String[] args) {
+		Deck deck = new Deck();
+	
 		BlackJackApp bj = new BlackJackApp();
+		List<Card> dealerHand = new ArrayList<>();
+		List<Card> playerHand = new ArrayList<>();
+		Scanner keyboard = new Scanner(System.in);
+		
 		System.out.println("Welcome to the Blackjack table");
 		System.out.println("Are you ready to play? Type (yes) or (no)");
-		Scanner keyboard = new Scanner(System.in);
 		String choice = keyboard.next();
-		List<Card> hand = new ArrayList<>();
 		if(choice.equals("yes")) {
-			for(int i = 0; i < 1; i++);
-			hand.add(deck.dealCard());
-			System.out.println(hand);
+			for( int i = 0; i < 1; i++) {
+				deck.shuffleDeck();
+				dealerHand.add(deck.dealCard());
+				System.out.println("The dealer has dealt two cards. ");
+				System.out.println("The card showing is " + dealerHand + "\n");
+				dealerHand.add(deck.dealCard());
+			
+			for (int j = 0; j < 2; j++) {
+				playerHand.add(deck.dealCard());
+			}
+			System.out.println("Your first two cards are " + playerHand);
+//			System.out.println(deck.checkDeckSize());
+			// figure out how to add the players cards.
+		
+		
+			
+				
+				
+				
+				
+			}
+			
 			
 		}
 		
@@ -32,10 +56,4 @@ public class BlackJackApp {
 		
 	}
 
-	
-		
-		}
-		
-	
-
-
+}
